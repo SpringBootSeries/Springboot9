378babcda49043cb83683d924ef0b7946742ab2e
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-28 20:49:39 +0530

    Created model classes

--- demo/src/main/java/com/example/demo/model/Employee.java
+++ demo/src/main/java/com/example/demo/model/Employee.java
@@ -1,4 +1,22 @@
 package com.example.demo.model;
 
-public class Employee {
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Employee extends Person {
+    @Id
+    private String empNo;
+    private double salary;
+    @ManyToOne
+    private Department department;
+    @ManyToMany(mappedBy = "employees")
+    private List<Project>projects;
 }
+
--- demo/src/main/java/com/example/demo/model/Project.java
+++ demo/src/main/java/com/example/demo/model/Project.java
@@ -1,4 +1,23 @@
 package com.example.demo.model;
 
+g
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+@Entity
 public class Project {
+    @Id
+    private int id;
+    private String name;
+    private long totalCost;
+    @ManyToMany
+    @JoinTable(name="project_assignments",
+            joinColumns = @JoinColumn(name="pro_id"),
+            inverseJoinColumns = @JoinColumn(name="emp_id"))
+    private List<Employee>employees;
 }
