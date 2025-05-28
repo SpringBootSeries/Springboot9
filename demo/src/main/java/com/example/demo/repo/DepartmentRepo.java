87df66a85c5667d9870fec82b272cfe467990f50
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-28 20:52:14 +0530

    Added repo

---
+++ demo/src/main/java/com/example/demo/repo/DepartmentRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.repo;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import lk.ac.vau.fas.ict.model.Department;
+
+@Repository
+public interface DepartmentRepo extends JpaRepository<Department, Integer> {
+
+}
+
